822df1eb5b128c14bc2fc0f7116560bff6189814
Danil IenbaIev <ennnbaev@icloud.com>
2021-08-16 23:23:16 +0300

    homework done

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> example=new ArrayList<>();
+        example.add("Hello");
+        example.add("my");
+        example.add("name");
+        example.add("is");
+        example.add("Danil");
+        System.out.println(deleteMore4(example));
+    }
+    public static List deleteMore4(List<String>example){
+        List<String>result=new ArrayList<>();
+        for(int i=example.size()-1;i>=0;i--){
+            if(example.get(i).length()<=4){
+                result.add(example.get(i));}
+        }
+        return result;
+    }
+}
