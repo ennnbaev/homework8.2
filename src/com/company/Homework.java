f9f15bb7a7b7d46b7b44346eb789e473ff43ece9
Danil IenbaIev <ennnbaev@icloud.com>
2021-08-16 23:29:10 +0300

    rename class

--- src/com/company/Homework.java
+++ src/com/company/Homework.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Main {
+public class Homework {
 
     public static void main(String[] args) {
         List<String> example=new ArrayList<>();
